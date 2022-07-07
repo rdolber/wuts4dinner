b5d335aa2f5f9cd8855093792ecabaa8ce69eae2
ray dolber <ray@stills.io>
2022-07-07 09:32:50 -0700

    initial project commit

---
+++ Wuts4Dinner/Wuts4Dinner/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Wuts4Dinner
+//
+//  Created by Ray Dolber on 7/7/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Wuts4Dinner/Wuts4Dinner/Wuts4DinnerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Wuts4DinnerApp.swift
+//  Wuts4Dinner
+//
+//  Created by Ray Dolber on 7/7/22.
+//
+
+import SwiftUI
+
+@main
+struct Wuts4DinnerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
